0719e645eeafb872e18e213b2bcda292a270edd1
YasminValim <yasmin.valimds@gmail.com>
2020-11-25 17:37:11 -0300

    New functions added!

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,18 @@
 class TodoList < ApplicationRecord
   has_many :todo_items
+ 
+  def percent_completed
+    return 0 if total_items == 0
+    ( 100 * completed_items.to_f / total_items)  
+  end
+
+  def completed_items 
+    @completed_items ||= todo_items.completed.count
+  end
+
+  def total_items
+    @total_items ||= todo_items.count
+  end
+  
+
 end
