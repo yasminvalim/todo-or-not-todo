79bbf18f2e94d67a86182ff776e49220e8112163
YasminValim <yasmin.valimds@gmail.com>
2020-11-25 18:27:26 -0300

    Stimulus Reflex Working!

--- app/reflexes/todo_item_reflex.rb
+++ app/reflexes/todo_item_reflex.rb
@@ -1,25 +1,16 @@
 # frozen_string_literal: true
 
 class TodoItemReflex < ApplicationReflex
-  # Add Reflex methods in this file.
-  #
-  # All Reflex instances expose the following properties:
-  #
-  #   - connection - the ActionCable connection
-  #   - channel - the ActionCable channel
-  #   - request - an ActionDispatch::Request proxy for the socket connection
-  #   - session - the ActionDispatch::Session store for the current visitor
-  #   - url - the URL of the page that triggered the reflex
-  #   - element - a Hash like object that represents the HTML element that triggered the reflex
-  #   - params - parameters from the element's closest form (if any)
-  #
-  # Example:
-  #
-  #   def example(argument=true)
-  #     # Your logic here...
-  #     # Any declared instance variables will be made available to the Rails controller and view.
-  #   end
-  #
-  # Learn more at: https://docs.stimulusreflex.com
+  
+  def mark_complete
+    todo_item = TodoItem.find(element.dataset.id)
+    todo_item.update(completed: true, completed_at: Time.now)
+  end
+  
+  def mark_incomplete
+    todo_item = TodoItem.find(element.dataset.id)
+    todo_item.update(completed: false, completed_at: nil)
+  end
 
+  
 end
